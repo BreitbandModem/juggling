e0ba62758ed3e7f51ac1cb5c934d9199cec9ee3d
Matthias <matthias.bidlingmeyer@gmail.com>
2020-04-30 23:30:45 +0100

    Dynamically apply brightness on slider change

--- app/static/index.js
+++ app/static/index.js
@@ -1,4 +1,16 @@
 $(document).ready(function() {
+    $('#brightness').on('change', function(event) {
+        $.ajax({
+            data :
+                {
+		    brightness : $('#brightness').val() 
+                },
+                type : 'POST',
+                url : '/'
+        });
+        event.preventDefault();
+    });
+	
     $('#calibrate_form').on('submit', function(event) {
         $.ajax({
             data :
